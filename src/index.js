f8c46efdec108615ea8d64cfddc3627bc3759eb1
arnesinnema <arnesinnema@hotmail.com>
2018-10-09 16:16:22 +0200

    Final commit before starting: Adding Time Travel section.

--- src/index.js
+++ src/index.js
@@ -46,6 +46,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();  // The slice() function creates a copy of the this.state.squares array
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;  // immediately return if there's a winner or the square is already filled
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -64,7 +68,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: '+winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -87,6 +97,7 @@ class Board extends React.Component {
       </div>
     );
   }
+
 }
 
 class Game extends React.Component {
@@ -105,7 +116,31 @@ class Game extends React.Component {
   }
 }
 
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i=0; i<lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+
 // ========================================
+
 
 ReactDOM.render(
   <Game />,
